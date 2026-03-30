dc366ec0b859dad7264396867bdf1f524f5cfdb1
Zhila <ministrov2018@gmail.com>
2026-03-30 14:32:16 +0300

    fix(eslintrc): fixing a eslintrc settings to new one

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+// .eslintrc.cjs
+module.exports = {
+  extends: ['htmlacademy'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'htmlacademy/vanilla',
+  rules: {
+    // You can override specific rules here if needed
+  },
+};
