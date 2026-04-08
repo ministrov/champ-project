695f2467d334b9489fd488ad6a3866e615d53185
Zhila <ministrov2018@gmail.com>
2026-04-08 14:26:56 +0300

    feat(js): create a validation scenarion for all inputs on form

--- src/form.js
+++ src/form.js
@@ -1,14 +1,15 @@
+/* eslint-disable no-console */
 export function initFormValidation() {
   const form = document.querySelector('.footer__form');
   let name = form.querySelector('input[name="user-name"]').value;
   let phone = form.querySelector('input[name="user-phone"]').value;
   let email = form.querySelector('input[name="user-email"]').value;
-  const inputField = form.querySelector('.footer-form__field');
+  const nameField = form.querySelector('.footer-form__field > input[name="user-name"]');
+  const phoneField = form.querySelector('.footer-form__field > input[name="user-phone"]');
+  const emailField = form.querySelector('.footer-form__field > input[name="user-email"]');
 
   const errorMessageBox = document.createElement('div');
   errorMessageBox.classList.add('footer-form__error-message');
-  inputField.append(errorMessageBox);
-
 
   form.addEventListener('submit', (e) => {
     let hasError = false;
@@ -18,24 +19,28 @@ export function initFormValidation() {
       hasError = true;
       errorMessage += 'Заполните поле "Имя"\n';
       errorMessageBox.textContent = errorMessage;
+      nameField.append(errorMessageBox);
     }
 
      if (phone === '') {
       hasError = true;
       errorMessage += 'Заполните поле "Телефон"\n';
       errorMessageBox.textContent = errorMessage;
+      phoneField.append(errorMessageBox);
     }
 
     if (email === '') {
       hasError = true;
       errorMessage += 'Заполните поле "Почта"\n';
       errorMessageBox.textContent = errorMessage;
+      emailField.append(errorMessageBox);
     }
 
     if (email !== '' && (!email.includes('@') || !email.includes('.'))) {
       hasError = true;
       errorMessage += 'Введите корректный email (должен содержать @ и точку)\n';
       errorMessageBox.textContent = errorMessage;
+      emailField.append(errorMessageBox);
     }
 
     // Если есть ошибки - отменяем отправку
@@ -44,7 +49,7 @@ export function initFormValidation() {
       // eslint-disable-next-line no-alert
       alert(errorMessage);
     }
-    // eslint-disable-next-line no-console
+
     console.log('form submitted');
   });
 
