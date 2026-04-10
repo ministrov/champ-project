b43643f095cac9cc0606afa02acaeb654eb3de2a
Zhila <ministrov2018@gmail.com>
2026-04-10 11:59:11 +0300

    fix(animations): reconnect AOS js library from npm install aos

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import { initMenu } from './burger-menu';
 import { initFormValidation } from './form';
+import AOS from 'aos';
 
 document.addEventListener('DOMContentLoaded', () => {
+  AOS.init({
+    duration: 700,
+    easing: 'ease-in-out',
+  });
+
   initMenu();
   initFormValidation();
 });
